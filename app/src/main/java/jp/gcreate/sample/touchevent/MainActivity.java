7f4ed8c9480eabf0a49fcf58712ad410d18870c2
Gen <archiherewego@gmail.com>
2016-02-02 17:59:55 +0900

    no data binding

--- app/src/main/java/jp/gcreate/sample/touchevent/MainActivity.java
+++ app/src/main/java/jp/gcreate/sample/touchevent/MainActivity.java
@@ -1,27 +1,39 @@
 package jp.gcreate.sample.touchevent;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
-
-import jp.gcreate.sample.touchevent.databinding.ActivityMainBinding;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
-    private ActivityMainBinding binding;
-    private MotionEventWrapper wrapper;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        wrapper = new MotionEventWrapper();
-        binding.setWrapper(wrapper);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        wrapper.setEvent(event);
-        return super.onTouchEvent(event);
+        int count = event.getPointerCount();
+        String text = "pointer count:" + count
+                + " x:" + event.getX()
+                + " y:" + event.getY() + "\n";
+        for (int i = 0; i < count; i++) {
+            try {
+                int id = event.getPointerId(i);
+                text += " pointer index:" + i + " pointer id:" + id
+                        + " x:" + event.getX(id)
+                        + " y:" + event.getY(id)
+                        + "\n";
+            }catch (IllegalArgumentException e){
+                e.printStackTrace();
+                text += " - pointer index:" + i + " out of range -\n";
+            }
+        }
+        textView.setText(text);
+        return true;
     }
 }
